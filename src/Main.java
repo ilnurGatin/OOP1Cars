9a7c4dbfbe843ab25d60fcfbcf6b0331eb4b2e8b
ilnurGatin <reilag@yandex.ru>
2022-10-18 02:06:12 +0500

    HW 1 Task 3

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        Car ladaGranta = new Car("Lada", "Granta", 1.7F, "Yellow", 2015, "Russia");
+        Car ladaGranta = new Car(null,null, 1.7F, "Yellow", 2015, "Russia");
         Car auduA8 = new Car("Audi", "A8 50 L TDI quattro", 3.0F, "Black", 2020, "Germany");
-        Car bmwZ8 = new Car("BMW", "Z8", 3.0F, "Black", 2021, "Germamy");
+        Car bmwZ8 = new Car("BMW", "Z8", "Germamy");
         Car kiaSportage = new Car("Kia", "Sportage 4th generation", 2.4F, "Red", 2018, "South-Korea");
-        Car hyundaiAvante = new Car("Hyundai", "Avante", 1.6F, "Orange", 2016, "South-Korea");
+        Car hyundaiAvante = new Car(1.6F, "Orange", 2016);
 
         ladaGranta.carInfo();
         auduA8.carInfo();
