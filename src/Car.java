df53dd24fc0d6f935c162c5ab18f6fe0a3b44cd6
ilnurGatin <reilag@yandex.ru>
2022-10-18 00:46:46 +0500

    HW 1 Task 3

---
+++ src/Car.java
@@ -0,0 +1,29 @@
+public class Car {
+
+    String brand;
+    String model;
+    float engineVolume;
+    String color;
+    int productionYear;
+    String productionCountry;
+
+    Car(String brand, String model, float engineVolume, String color, int productionYear, String productionCountry) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.productionYear = productionYear;
+        this.productionCountry = productionCountry;
+    }
+
+    void carInfo() {
+        System.out.println("Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", engineVolume=" + engineVolume +
+                ", color='" + color + '\'' +
+                ", productionYear=" + productionYear +
+                ", productionCountry='" + productionCountry + '\'' +
+                '}');
+    }
+}
