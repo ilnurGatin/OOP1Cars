1b91742479a0d9fc2ed847dd595a4040c85622ae
ilnurGatin <reilag@yandex.ru>
2022-10-21 01:04:15 +0500

    HW 3

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,34 @@
+import transport.Car;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
 
-        Car ladaGranta = new Car(null,null, 1.7F, "Yellow", 2015, "Russia");
-        Car auduA8 = new Car("Audi", "A8 50 L TDI quattro", 3.0F, "Black", 2020, "Germany");
-        Car bmwZ8 = new Car("BMW", "Z8", "Germamy");
-        Car kiaSportage = new Car("Kia", "Sportage 4th generation", 2.4F, "Red", 2018, "South-Korea");
-        Car hyundaiAvante = new Car(1.6F, "Orange", 2016);
+        Car ladaGranta = new Car(null,null, 1.7F, "Yellow", 2015,
+                "Russia", "Manual", "Sedan",
+                "A777MP777", 4, true, new Car.Key(true,true),
+                new Car.Insurance(LocalDate.now(), 10,"578767336"));
+        Car audiA8 = new Car("Audi", "R8", 3.0F, "Black", 2018,
+                "Germany", "Auto", "Hatchback", "F778MH789",
+                4, false, new Car.Key(true,true), new Car.Insurance(LocalDate.now(),
+                10,"578767336"));
+        Car bmwZ8 = new Car("BMW", "Z8", 5.0f, "Black",2015, "Germamy",
+                 "Manual", "Sedan", "D555AS175", 2, true,
+                new Car.Key(true,true), new Car.Insurance(LocalDate.now(), 10,"578767336"));
+        Car kiaSportage = new Car("Kia", "Sportage 4th generation", 2.4F, "Red", 2018,
+                "South-Korea", "Manual", "Sedan", "555", 5,
+                 false, new Car.Key(true,true),
+                new Car.Insurance(LocalDate.now(), 10,"578767336"));
+        Car hyundaiAvante = new Car(null, null, 1.6F, "Orange", 2016,
+                "Korea", "Auto", "Hatchback", null, 0,
+                false, new Car.Key(true,true),
+                new Car.Insurance(LocalDate.now(), 10,"578767336"));
 
-        ladaGranta.carInfo();
-        auduA8.carInfo();
-        bmwZ8.carInfo();
-        kiaSportage.carInfo();
-        hyundaiAvante.carInfo();
+        System.out.println(ladaGranta);
+        System.out.println(audiA8);
+        System.out.println(bmwZ8);
+        System.out.println(kiaSportage);
+        System.out.println(hyundaiAvante);
     }
 }
